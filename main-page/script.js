45f78a51f39b89632ea55f1cc18715c0b3ee85c4
Aleksandr Anseev <anse0012@algonquinlive.com>
2020-11-14 20:37:48 -0500

    Skills added
    Skills section is complete

--- main-page/script.js
+++ main-page/script.js
@@ -10,8 +10,9 @@ for (var i = 0; i < links.length; i++) {
 
 var i = 0;
 var txt = 'I am Aleksandr Anseev, Computer Programming student in Canada, Ottawa. I have experience in building web-sites and developing software. More information about my skills and epxerience you can find below!';
-var speed = 50;
+var speed = 15;
 var btnOpacity = 1;
+var hireOpacity = 0;
 
 function typeWriter() {
   if (i < txt.length) {
@@ -22,5 +23,25 @@ function typeWriter() {
         btnOpacity -= 0.1;
         document.getElementById("typeBtn").style.opacity = btnOpacity;
     }
+  } else {
+      if(hireOpacity < 1) {
+        hireOpacity += 0.1;
+        document.getElementById("hire").style.opacity = hireOpacity;
+        setTimeout(typeWriter, speed);
+      }
   }
 }
+
+function move(barName, width) {
+    var elem = document.getElementById(barName);   
+    var id = setInterval(frame, 30);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
